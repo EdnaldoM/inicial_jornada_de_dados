fe828ef68a53baacb945725d32bc4dbbb542a103
ednaldom <ednaldo.pereira@indicium.tech>
2025-07-27 20:10:26 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 31
+nome = "Ednaldo"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade +1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+
+print(lista)
+print(lista_invertida)
